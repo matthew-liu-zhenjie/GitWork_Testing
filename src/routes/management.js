2b74255a9689fdaa6b387d7d0daa1383dcd8b71d
matthew-liu-zhenjie <matthew.liu.zhenjie@gmail.com>
2022-05-21 11:55:23 +0800

    Revert "Testing more stuff" (#2)

--- src/routes/management.js
+++ src/routes/management.js
@@ -1,6 +1,6 @@
 export default function management() {
   return (
-    <div className="Management">
+    <div classname="Management">
       <h1> Management Page </h1>
       <h3> Hello Lord of the workplace </h3>
     </div>
